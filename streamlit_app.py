964799c6ace26f0f00d3e9c8503f4fc3b5522a2b
nanand2k24 <nanand.2k24@gmail.com>
2024-05-16 00:59:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-import requests
 from snowflake.snowpark.functions import col
 
 
@@ -36,7 +35,9 @@ if ingredient_list:
     if time_to_submit:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered! ' +name_on_order, icon="✅")
-    
+
+import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
 fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
